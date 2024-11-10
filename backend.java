d1a12c950d7258f8bb9abd56ae5841beda9d3c1b
BharathKumar257 <rmbharathk@gmail.com>
2024-11-10 12:44:17 +0530

    fix : changed code

--- backend.java
+++ backend.java
@@ -1,1 +1,9 @@
-public 
+public class Backend{
+  public void back(){
+    System.out.println("Backend")
+}
+  Public static void main (String [] args){
+    Backend b = new Backend();
+    b.back();
+  }
+}
