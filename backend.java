0be78c0e013674cdb9615e4f47860c15f42f817a
BharathKumar257 <rmbharathk@gmail.com>
2024-11-10 12:37:02 +0530

    feat:added a code

--- backend.java
+++ backend.java
@@ -1,9 +1,1 @@
-public class Backend{
-  public void back(){
-    System.out.println("Backend")
-}
-  Public static void main (String [] args){
-    Backend b = new Backend();
-    b.back();
-  }
-}
+public 
